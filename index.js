92b61c9c17128b6a7b2fd5cde0437a9d6498e39e
wadad zein <wadad_zein@hotmail.com>
2021-04-14 09:52:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ let leashDog=function (dogName, dogBreed){
 	let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
 	
 	function exerciseDog(dog, breed){
-	  return routine.map(fn=>fn(dog,breed))
-	}
+	  return routine.map(fn=>fn(dog,breed));
 	}
 	
